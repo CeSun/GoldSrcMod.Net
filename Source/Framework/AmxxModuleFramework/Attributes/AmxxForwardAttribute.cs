12ad8c5da58562853f025921a5994f49569b6266
CeSun <ce.sun@outlook.com>
2024-10-07 05:24:27 +0800

    forward attribute

---
+++ Source/Framework/AmxxModuleFramework/Attributes/AmxxForwardAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldSrc.Amxmodx.Framework.Attributes;
+
+public class AmxxForwardAttribute : Attribute
+{
+    public string Name { get; set; }
+    public AmxxForwardAttribute(string Name)
+    {
+        this.Name = Name;
+    }
+    public AmxxForwardAttribute()
+    {
+        this.Name = "";
+    }
+}
+
